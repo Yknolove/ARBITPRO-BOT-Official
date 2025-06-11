06a1dab275d3470b83df66e56c6d29af3ddd1407
Yknolove <giffka9@gmail.com>
2025-06-11 15:44:32 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -5,6 +5,9 @@ load_dotenv()
 
 API_TOKEN = os.getenv('TELEGRAM_TOKEN')
 WEBHOOK_PATH = os.getenv('WEBHOOK_PATH', '/webhook')
-WEBHOOK_URL = os.getenv('WEBHOOK_URL')  # https://your-domain.com + WEBHOOK_PATH
+WEBHOOK_URL = os.getenv('WEBHOOK_URL')
 DB_URL = os.getenv('DATABASE_URL', 'sqlite+aiosqlite:///./db.sqlite')
 TRON_API_KEY = os.getenv('TRON_API_KEY')
+
+# Новый блок:
+PORT = int(os.getenv('PORT', 8000))
