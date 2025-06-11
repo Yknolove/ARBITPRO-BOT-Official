00db59c9ac3dcd7f2326f11fccf21cdc2ad172eb
Yknolove <giffka9@gmail.com>
2025-06-11 22:29:49 +0300

    Update user_setting.py

--- models/user_setting.py
+++ models/user_setting.py
@@ -1,12 +1,11 @@
 from sqlalchemy import Column, Integer, String, Float
- from config.db import Base
- 
- class UserSetting(Base):
-     __tablename__ = "user_settings"
- 
-     user_id = Column(Integer, primary_key=True, index=True)
-     exchange = Column(String, nullable=False, default="binance")
-     buy_threshold = Column(Float, nullable=True)
-     sell_threshold = Column(Float, nullable=True)
--    #volume_limit missing
-+    volume_limit = Column(Float, nullable=True)  # Лимит объёма сделки
+from config.db import Base
+
+class UserSetting(Base):
+    __tablename__ = "user_settings"
+
+    user_id = Column(Integer, primary_key=True, index=True)
+    exchange = Column(String, nullable=False, default="binance")
+    buy_threshold = Column(Float, nullable=True)
+    sell_threshold = Column(Float, nullable=True)
+    volume_limit = Column(Float, nullable=True)
