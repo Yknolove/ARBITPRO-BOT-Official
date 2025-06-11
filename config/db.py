b47dd333b61de723e73431fded0242daf0595a35
Yknolove <giffka9@gmail.com>
2025-06-11 14:42:34 +0300

    Create db.py

---
+++ config/db.py
@@ -0,0 +1,9 @@
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
+from sqlalchemy.orm import sessionmaker, declarative_base
+from config.config import DATABASE_URL
+
+engine = create_async_engine(DATABASE_URL, echo=False)
+AsyncSessionLocal = sessionmaker(
+    bind=engine, class_=AsyncSession, expire_on_commit=False
+)
+Base = declarative_base()
