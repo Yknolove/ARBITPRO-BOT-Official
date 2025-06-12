097d786f50e914db46e5617cb84413c53f02cbc2
Yknolove <giffka9@gmail.com>
2025-06-12 20:23:37 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-API_TOKEN = os.getenv("TELEGRAM_TOKEN")
+API_TOKEN = os.getenv(8131766932:AAFPfxgWtoY7fejhp5dofLsz0q7701L4GAI)
 WEBHOOK_PATH = os.getenv("WEBHOOK_PATH", "/webhook")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 PORT = int(os.getenv("PORT", 8000))
