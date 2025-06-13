be171cb99ab6f8a1de10f58a60557b1e34f5e216
Yknolove <giffka9@gmail.com>
2025-06-13 14:07:34 +0300

    Delete config directory

--- config/__init__.py
+++
@@ -1,9 +0,0 @@
-import os
-
-# Забираем токен из переменных окружения
-API_TOKEN = os.getenv("API_TOKEN")
-if not API_TOKEN:
-    raise RuntimeError("❗ Переменная окружения API_TOKEN не установлена")
-
-# Порт (необязательно для polling, но может понадобиться)
-PORT = int(os.getenv("PORT", 8000))
