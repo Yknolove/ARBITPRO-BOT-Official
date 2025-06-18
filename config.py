21033677b407b9b7af7181363340a6894a668c57
Yknolove <giffka9@gmail.com>
2025-06-18 10:51:26 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,5 @@
-# config.py
 import os
 
-API_TOKEN = os.getenv("8131766932:AAFPfxgWtoY7fejhp5dofLsz0q7701L4GAI")
-WEBHOOK_URL = os.getenv("https://arbitpro-bot-official.onrender.com/webhook")
+API_TOKEN = os.getenv("API_TOKEN")
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
 PORT = int(os.getenv("PORT", 10000))
