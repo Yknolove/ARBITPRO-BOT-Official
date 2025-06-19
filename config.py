1763bfead0bf2f64d964e98b2ac28fbcc1a5d51b
Yknolove <giffka9@gmail.com>
2025-06-19 17:42:37 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 FILTERS_FILE = "filters.json"
 API_TOKEN = os.getenv("API_TOKEN")
 WEBHOOK_URL = os.getenv("WEBHOOK_URL")
-PORT = int(os.getenv("PORT", 10000))
+WEBAPP_PORT = int(os.getenv("PORT", 10000))
